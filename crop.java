55edb65c05ce7dad8f7db732da8d2ef493163b3b
ghanshyamypsilon <ghanshyamypsilon@users.noreply.github.com>
2016-01-25 18:37:03 +0530

    Create crop.java

---
+++ crop.java
@@ -0,0 +1,90 @@
+private void doCrop(Context mContext,) {
+		
+		final ArrayList<CropOption> cropOptions = new ArrayList<CropOption>();
+
+		Intent intent = new Intent("com.android.camera.action.CROP");
+		intent.setType("image/*");
+
+		List<ResolveInfo> list = mContext.getPackageManager().queryIntentActivities(
+				intent, 0);
+
+		int size = list.size();
+
+		if (size == 0) {
+			Toast.makeText(mContext, "Can not find image crop app",
+					Toast.LENGTH_SHORT).show();
+
+			return;
+		} else {
+			
+			intent.setData(mImageCaptureUri);
+
+			intent.putExtra("outputX", 200);
+			intent.putExtra("outputY", 200);
+			intent.putExtra("aspectX", 1);
+			intent.putExtra("aspectY", 1);
+			intent.putExtra("scale", true);
+			intent.putExtra("return-data", true);
+
+			if (size == 1) {
+				
+				Intent i = new Intent(intent);
+				ResolveInfo res = list.get(0);
+
+				i.setComponent(new ComponentName(res.activityInfo.packageName,
+						res.activityInfo.name));
+
+				startActivityForResult(i,CROP_FROM_CAMERA);
+				
+			} else {
+				
+				for (ResolveInfo res : list) {
+					
+					final CropOption co = new CropOption();
+
+					co.title = mContext.getPackageManager().getApplicationLabel(
+							res.activityInfo.applicationInfo);
+					co.icon = mContext.getPackageManager().getApplicationIcon(
+							res.activityInfo.applicationInfo);
+					co.appIntent = new Intent(intent);
+
+					co.appIntent
+							.setComponent(new ComponentName(
+									res.activityInfo.packageName,
+									res.activityInfo.name));
+
+					cropOptions.add(co);
+				}
+
+				CropOptionAdapter adapter = new CropOptionAdapter(
+						mContext, cropOptions);
+
+				AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+				builder.setTitle("Choose Crop App");
+				builder.setAdapter(adapter,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int item) {
+								startActivityForResult(
+										cropOptions.get(item).appIntent,
+										CROP_FROM_CAMERA);
+							}
+						});
+
+				builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+					@Override
+					public void onCancel(DialogInterface dialog) {
+
+						if (mImageCaptureUri != null) {
+							mContext.getContentResolver().delete(mImageCaptureUri, null,
+									null);
+							mImageCaptureUri = null;
+						}
+					}
+				});
+
+				AlertDialog alert = builder.create();
+
+				alert.show();
+			}
+		}
+	}
